5cc95f96ccbdb9bd3e796164d307119bec0e3519
Andrei Marcu <animar71@gmail.com>
2024-10-30 20:23:08 +0100

    Initial Commit

---
+++ Places/PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesApp.swift
+//  Places
+//
+//  Created by Andrei Marcu on 30.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlacesTests/PlacesTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesTests.swift
+//  PlacesTests
+//
+//  Created by Andrei Marcu on 30.10.2024.
+//
+
+import Testing
+@testable import Places
+
+struct PlacesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
